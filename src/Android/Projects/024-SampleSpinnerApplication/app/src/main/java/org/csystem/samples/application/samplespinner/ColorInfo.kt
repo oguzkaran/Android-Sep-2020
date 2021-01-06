f7dcb25264781a5150e0c0d513913fe924ed4c38
Oğuz Karan <oguzkaran@csystem.org>
2021-01-06 22:56:14 +0300

    Android

---
+++ src/Android/Projects/024-SampleSpinnerApplication/app/src/main/java/org/csystem/samples/application/samplespinner/ColorInfo.kt
@@ -0,0 +1,5 @@
+package org.csystem.samples.application.samplespinner
+
+data class ColorInfo(var color: Int, var name: String) {
+    override fun toString() = name
+}
---
+++ src/Android/Projects/024-SampleSpinnerApplication/app/src/main/java/org/csystem/samples/application/samplespinner/MainActivity.kt
@@ -0,0 +1,97 @@
+package org.csystem.samples.application.samplespinner
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener{
+    private lateinit var mColors: Array<ColorInfo>
+    private lateinit var mLinearLayoutMain: LinearLayout
+    private lateinit var mTextViewColor: TextView
+    private lateinit var mSpinnerColors: Spinner
+
+    private fun showAboutDialog()
+    {
+        AlertDialog.Builder(this).apply {
+            setTitle(R.string.about_title)
+            setMessage(R.string.about_message)
+            setPositiveButton(R.string.about_positive_button) {_, _->}
+        }.show()
+    }
+
+    private fun initColorSpinner()
+    {
+        mSpinnerColors = findViewById(R.id.mainActivitySpinnerColors)
+        mSpinnerColors.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, mColors)
+        mSpinnerColors.onItemSelectedListener = this
+    }
+
+    private fun initViews()
+    {
+        mLinearLayoutMain = findViewById(R.id.mainActivityLinearLayoutMain)
+        initColorSpinner()
+        mTextViewColor = findViewById(R.id.mainActivityTextViewColor)
+    }
+
+    private fun initColors()
+    {
+        mColors = arrayOf(
+            ColorInfo(getColor(R.color.white), "Beyaz"),
+            ColorInfo(getColor(R.color.red), "Kırmızı"),
+            ColorInfo(getColor(R.color.green), "Yeşil"),
+            ColorInfo(getColor(R.color.blue), "Mavi"),
+        )
+    }
+
+    private fun initialize()
+    {
+        initColors()
+        initViews()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initialize()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean
+    {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, pos: Int, p3: Long)
+    {
+        mLinearLayoutMain.setBackgroundColor(mColors[pos].color)
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?)
+    {
+        Toast.makeText(this, "Nothing selected", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean
+    {
+        when (item.itemId) {
+            R.id.mainActivityMenuItemAbout -> showAboutDialog()
+            R.id.mainActivityMenuItemExit -> finish()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun onOKButtonClicked(view: View)
+    {
+        val colorInfo = mSpinnerColors.selectedItem as ColorInfo
+
+        mTextViewColor.setBackgroundColor(Color.rgb(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256)))
+        "Zemin rengi:${colorInfo.name}".also { mTextViewColor.text = it }
+    }
+}
