a03eb794cb6806ce4b271fc44b00fc2c0b8d8960
Oğuz Karan <oguzkaran@csystem.org>
2021-02-05 22:42:28 +0300

    Android-Sep-2020

--- src/Android/Projects/032-SampleApplicationWithTest/app/src/test/java/org/csystem/samples/application/samplewithtest/FactorialTest.kt
+++ src/Android/Projects/032-SampleApplicationWithTest/app/src/test/java/org/csystem/samples/application/samplewithtest/FactorialTest.kt
@@ -5,7 +5,6 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
-import java.io.BufferedReader
 import java.nio.file.Files
 import java.nio.file.Paths
 
@@ -14,23 +13,13 @@ class FactorialTest(private val mDataInfo: DataInfo) {
     class DataInfo(val a: Int, val result: Long)
 
     companion object {
-        @JvmStatic
-        private fun fillDataInfo(br: BufferedReader, data: MutableCollection<DataInfo>)
-        {
-            while (true) {
-                val line = br.readLine() ?: break
-                val dataInfoStr = line.split(',')
-                data.add(DataInfo(dataInfoStr[0].toInt(), dataInfoStr[1].toLong()))
-            }
-        }
-
         @Parameterized.Parameters
         @JvmStatic
         fun provideData(): MutableCollection<DataInfo> {
             val data = ArrayList<DataInfo>()
 
-            Files.newBufferedReader(Paths.get("setup-factorial.txt")).use {
-                fillDataInfo(it, data)
+            Files.newBufferedReader(Paths.get("setup-factorial.txt")).useLines {
+                it.map { it.split(',') }.map { DataInfo(it[0].toInt(), it[1].toLong()) }.forEach { data.add(it) }
             }
 
             return data
@@ -38,7 +27,7 @@ class FactorialTest(private val mDataInfo: DataInfo) {
     }
 
     @Test
-    fun testIsPrime()
+    fun testFactorial()
     {
         assertEquals(mDataInfo.result, NumberUtil.factorial(mDataInfo.a))
     }
--- src/Android/Projects/032-SampleApplicationWithTest/app/src/test/java/org/csystem/samples/application/samplewithtest/IsPrimeTest.kt
+++ src/Android/Projects/032-SampleApplicationWithTest/app/src/test/java/org/csystem/samples/application/samplewithtest/IsPrimeTest.kt
@@ -5,7 +5,6 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
-import java.io.BufferedReader
 import java.nio.file.Files
 import java.nio.file.Paths
 
@@ -14,23 +13,13 @@ class IsPrimeTest(private val mDataInfo: DataInfo) {
     class DataInfo(val a: Int, val result: Boolean)
 
     companion object {
-        @JvmStatic
-        private fun fillDataInfo(br: BufferedReader, data: MutableCollection<DataInfo>)
-        {
-            while (true) {
-                val line = br.readLine() ?: break
-                val dataInfoStr = line.split(' ')
-                data.add(DataInfo(dataInfoStr[0].toInt(), dataInfoStr[1].toBoolean()))
-            }
-        }
-
         @Parameterized.Parameters
         @JvmStatic
         fun provideData(): MutableCollection<DataInfo> {
             val data = ArrayList<DataInfo>()
 
-            Files.newBufferedReader(Paths.get("setup-prime.txt")).use {
-                fillDataInfo(it, data)
+            Files.newBufferedReader(Paths.get("setup-prime.txt")).useLines {
+                it.map { it.split(' ') }.map {DataInfo(it[0].toInt(), it[1].toBoolean())}.forEach { data.add(it) }
             }
 
             return data
