5864cf1babd8c45f170549f6ca31b056f721e3aa
Oğuz Karan <oguzkaran@csystem.org>
2021-04-12 23:01:25 +0300

    Android-Sep-2020

--- src/Java-Kotlin/Sample/src/main/java/org/csystem/app/app.kt
+++ src/Java-Kotlin/Sample/src/main/java/org/csystem/app/app.kt
@@ -1,39 +1,31 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    Aşağıdaki örnekte bir couritine içerisinde başka bir coroutine yaratılmış, ve runBlocking ile yaratılan
-    couroutine doWork içerisinde yaratılan coroutine'i beklemektedir. Bu durumda derleyici ve "Virtual machine" optimize
-    ederek couroutine sayısını düşürebilir
+    Aşağıdaki örnekte coroutine içerisindeki fonksiyonun geri dönüş değeri elde edilmiştir
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app
 
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import org.csystem.util.readInt
-import kotlin.random.Random
+import kotlinx.coroutines.*
 
-suspend fun main() = run().apply { println("run çağrıldı") }.join()
-
-fun doWork()  = runBlocking {
-    var sum = 0
-    val n = readInt("Bir sayı giriniz:")
-
-    val job = GlobalScope.launch {
-        for (i in 1..n) {
-            val value = Random.nextInt(100)
-            print("$value ")
-            sum += value
-            delay(Random.nextLong(1, 1200))
-        }
-        println()
+fun main() {
+    runBlocking {
+        doWork()
+        println("Hello World")
+        readLine()
     }
-
-    job.join()
-    println("Toplam:$sum")
 }
 
-fun run() =
+suspend fun doWork()
+{
     GlobalScope.launch {
-        doWork()
+        getResult()
     }
+}
+suspend fun getResult() = coroutineScope {
+    println("result = ${async(Dispatchers.Main) { foo() }.await()}")
+    println("Hello...")
+}
+suspend fun foo() : Int
+{
+    delay(3000)
+    return 10
+}
 
